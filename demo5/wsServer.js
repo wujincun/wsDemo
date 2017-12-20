07b6259f310db8291b52d6d89fc4d1a9624ae2d1
wujincun <1769453779@qq.com>
2017-12-20 14:13:41 +0800

    socket。io

---
+++ demo5/wsServer.js
@@ -0,0 +1,14 @@
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+
+app.listen(8080);
+
+
+io.on('connection', function(socket) {
+	socket.emit('news', {
+		hello: 'world'
+	});
+	socket.on('my other event', function(data) {
+		console.log(data);
+	});
+});
